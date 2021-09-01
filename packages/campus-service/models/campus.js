ee1555ff52888b2b4201abde8e3b13427e165db5
PushpakB3096 <rtpushpak@gmail.com>
2021-09-01 21:15:09 +0530

    Add campus model

--- packages/campus-service/models/campus.js
+++ packages/campus-service/models/campus.js
@@ -1,6 +1,17 @@
 import mongoose from "mongoose";
 
 // defining a schema for how a cammpus will look like
-const campusSchema = mongoose.Schema({});
+const campusSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  admin: {
+    type: String,
+    required: true,
+    unique: false
+  }
+});
 
 export default mongoose.model("Campus", campusSchema);
