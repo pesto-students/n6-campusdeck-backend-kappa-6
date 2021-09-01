5f395d9136ae78f9b5e491ecd4f876da84b1e5f9
PushpakB3096 <rtpushpak@gmail.com>
2021-09-01 18:28:27 +0530

    Add login functionality

--- packages/user-service/controllers/users.js
+++ packages/user-service/controllers/users.js
@@ -68,3 +68,51 @@ export const signUp = async (req, res) => {
     });
   }
 };
+
+export const signIn = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    // gets at most one user because email is unique
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      return res.status(404).json({
+        message: `User with email: ${email} doesn't exist`
+      });
+    }
+
+    // if user does exist, we need to check whether the password matches or not
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    /* if we bypass both the conditions above, that means that we have a correct user logging in
+        in which case, we will create and send the jwt to the client
+    */
+    const token = jwt.sign(
+      {
+        email: existingUser.email,
+        id: existingUser._id
+      },
+      process.env.SECRET,
+      { expiresIn: "1h" }
+    );
+
+    return res.status(200).json({
+      result: existingUser,
+      status: "success",
+      token
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "error",
+      message: `Something went wrong. ${error}`
+    });
+  }
+};
