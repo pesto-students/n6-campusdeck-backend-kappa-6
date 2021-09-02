a9249c0aab62a7a4994a842bfb9f205357ccc26d
PushpakB3096 <rtpushpak@gmail.com>
2021-09-02 17:10:10 +0530

    Add route for fetching campus

--- packages/campus-service/controllers/campus.js
+++ packages/campus-service/controllers/campus.js
@@ -1,12 +1,27 @@
 import Campus from "../models/campus.js";
 
 export const createCampus = async (req, res) => {
-  const result = await Campus.create({
-    name: "VIT, Vellore",
-    admin: "rtpushpak@gmail.com "
-  });
+  // const result = await Campus.create({
+  //   name: "VIT, Vellore",
+  //   admin: "rtpushpak@gmail.com "
+  // });
 
   res.status(200).send({
     message: "Campus created successfully"
   });
+};
+
+export const getAllCampus = async (req, res) => {
+  try {
+    const allCampus = await Campus.find().sort({ _id: "desc" });
+
+    res.status(200).json({
+      status: "success",
+      data: allCampus
+    });
+  } catch (err) {
+    res.status(404).json({
+      message: err.message
+    });
+  }
 };
