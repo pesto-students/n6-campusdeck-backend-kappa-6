7018ecf73dfedfe4cc5e8617b32d813d3b4c48e0
PushpakB3096 <rtpushpak@gmail.com>
2021-09-02 16:22:58 +0530

    Add route to fetch all campus

--- packages/campus-service/routes/campus.js
+++ packages/campus-service/routes/campus.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { createCampus } from "../controllers/campus.js";
+import { createCampus, getAllCampus } from "../controllers/campus.js";
 
 const router = express.Router();
 
+router.get("/", getAllCampus);
 router.post("/", createCampus);
 
 export default router;
